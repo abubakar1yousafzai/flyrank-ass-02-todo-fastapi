60473e1edc9104eee113476c9baac2799edbe2ab
Abu Bakar <contact1abubakarkhan@gmail.com>
2026-07-18 08:34:41 +0500

    create a put endpoint for update a task

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Response, HTTPException
 from pydantic import BaseModel
+from typing import Optional
 import uvicorn
 
 tasks = [
@@ -11,6 +12,11 @@ tasks = [
 class TaskCreate(BaseModel):
     title: str
     done: bool = False
+
+class UpdateTask(BaseModel):
+    title: Optional[str] = None
+    done: Optional[bool] = None
+
 
 app = FastAPI(
     title= "CRUD Todo",
@@ -44,6 +50,7 @@ def get_task(task_id: int):
 
 @app.post("/tasks", status_code=201)
 def create_task(task: TaskCreate):
+    
     if not task.title.strip():
         raise HTTPException(status_code=400, detail="Title cannot be empty")
 
@@ -51,6 +58,20 @@ def create_task(task: TaskCreate):
     new_task = {"id": new_id, "title": task.title, "done": task.done}
     tasks.append(new_task)
     return new_task
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated: UpdateTask):
+    for task in tasks:
+        if task["id"] == task_id:
+            if updated.title is not None:
+                title = updated.title.strip()
+                if not title:
+                    raise HTTPException(status_code=400, detail="Title cannot be empty")
+                task["title"] = title
+            if updated.done is not None:
+                task["done"] = updated.done
+            return task
+    raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
